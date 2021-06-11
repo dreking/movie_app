8fd2f42967d5bb0fce6470dae7fbd4dba4911c3d
Leandre <leajapo@gmail.com>
2021-06-11 20:49:04 +0200

    feat&impr: imprvoed code, registered favorite screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movie_app/screens/favorites_screen.dart';
 import 'package:provider/provider.dart';
 
 import 'package:movie_app/providers/movie_logic.dart';
@@ -21,14 +22,29 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider.value(value: MovieLogic()),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Film Fan',
         theme: ThemeData(
           primarySwatch: Colors.blue,
+          primaryColor: Colors.blue,
+          textTheme: TextTheme(
+            headline1: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            headline2: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+            headline3: TextStyle(
+              fontSize: 18,
+            ),
+          ),
         ),
         routes: {
           MainScreen.routeName: (ctx) => MainScreen(),
           MovieDetailsScreen.routeName: (ctx) => MovieDetailsScreen(),
           MovieCastScreen.routeName: (ctx) => MovieCastScreen(),
+          FavoriteMoviesScreen.routeName: (ctx) => FavoriteMoviesScreen(),
         },
       ),
     );
